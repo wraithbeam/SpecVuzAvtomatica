35b7c8bfd11c0f12cb60cf7713c5a1110edc9c18
Vlad Eremeev <eremeev.vlad.v@yandex.ru>
2022-06-17 22:13:33 +0300

    Добавлен пул потоков

--- src/main/java/com/wraithbeam/specvuzavtomatica/DirectoryAnalyzer.java
+++ src/main/java/com/wraithbeam/specvuzavtomatica/DirectoryAnalyzer.java
@@ -1,20 +1,24 @@
 package com.wraithbeam.specvuzavtomatica;
 
 import java.io.File;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DirectoryAnalyzer {
     private File directory;
+    private int numberOfThreads = 15;
 
     public DirectoryAnalyzer(File directory) {
         this.directory = directory;
     }
 
     public void analyse(){
+        ExecutorService pool = Executors.newFixedThreadPool(numberOfThreads);
         for (File file: directory.listFiles()) {
             if (file.isFile()) {
                 Zipper zipper = new Zipper(file);
-                Thread thread = new Thread(zipper);
-                thread.start();
+                pool.execute(zipper);
             }
             if (file.isDirectory()){
                 DirectoryAnalyzer directoryAnalyzer = new DirectoryAnalyzer(file);
