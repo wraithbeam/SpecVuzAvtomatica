2382dc818aa34890c32ef20a7cf869cd7f0ea077
Vlad Eremeev <eremeev.vlad.v@yandex.ru>
2022-06-24 21:44:01 +0300

    Simple progress listener

--- src/main/java/com/wraithbeam/specvuzavtomatica/DirectoryAnalyzer.java
+++ src/main/java/com/wraithbeam/specvuzavtomatica/DirectoryAnalyzer.java
@@ -2,14 +2,15 @@ package com.wraithbeam.specvuzavtomatica;
 
 import java.io.File;
 import java.util.Objects;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class DirectoryAnalyzer {
     private File directory;
     private static final int numberOfThreads = 15;
     private static final ExecutorService pool = Executors.newFixedThreadPool(numberOfThreads);
+    private int countOfFiles = 0;
 
     public DirectoryAnalyzer(File directory) {
         this.directory = directory;
@@ -20,12 +21,23 @@ public class DirectoryAnalyzer {
             if (file.isFile()) {
                 Zipper zipper = new Zipper(file);
                 pool.execute(zipper);
+                countOfFiles++;
             }
             if (file.isDirectory()){
                 DirectoryAnalyzer directoryAnalyzer = new DirectoryAnalyzer(file);
                 directoryAnalyzer.analyse();
             }
         }
+        while (countOfFiles != Zipper.getCountOfCompletedFiles()){
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+                System.out.println(Zipper.getCountOfCompletedFiles());
+            }
+            catch (InterruptedException e){
+                System.out.println("Application was interrupted");
+            }
+        }
+        System.out.println("Task completed");
     }
 
 
